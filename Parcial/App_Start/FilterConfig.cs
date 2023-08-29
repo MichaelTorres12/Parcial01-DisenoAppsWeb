6923127561c2746914d8183b9b57c0bedd8cde69
Gabyta69 <ia.rc101320@ufg.edu.sv>
2023-08-29 17:50:00 -0600

    hrfthtr

--- Parcial/App_Start/FilterConfig.cs
+++ Parcial/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace Parcial
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+          
         }
     }
 }
---
+++ Parcial/Controllers/AccessController.cs
@@ -0,0 +1,52 @@
+﻿using Parcial.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Parcial.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string user, string password)
+        {
+            try
+            {
+                using (RC101320Entities db = new RC101320Entities())
+                {
+                    var hashedPassword = BCrypt.Net.BCrypt.HashPassword(password); // Hashea la contraseña ingresada
+
+                    var userInDb = db.Estudiantes.FirstOrDefault(u => u.email == user && u.password == password);
+
+                    if (userInDb != null && BCrypt.Net.BCrypt.Verify(password, userInDb.password))
+                    {
+                       return RedirectToAction("Index","Home");    
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "usuario o contraseña invalido";
+                        return View();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones, registra el error o realiza otras acciones necesarias.
+                Debug.WriteLine("Error en el inicio de sesión: " + ex.Message);
+                return Content("Error en el inicio de sesión");
+            }
+
+          
+        }
+
+    }
+}
