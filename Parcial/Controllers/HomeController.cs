ff3a9f66b64510990aaa54abd450e1c5ac991ba2
Michael Torres <138163445+MichaelTorres12@users.noreply.github.com>
2023-09-02 10:20:04 -0600

    Update HomeController.cs

--- Parcial/Controllers/HomeController.cs
+++ Parcial/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace Parcial.Controllers
 {
     public class HomeController : Controller
     {
-        private RC101320Entities1 db = new RC101320Entities1();
+        private TC100220Parcial01 db = new TC100220Parcial01();
 
         public ActionResult Index()
         {
